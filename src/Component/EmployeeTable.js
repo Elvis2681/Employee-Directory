57f5cd973ba4d2fe281c0998381d8df38348496e
Elvis Lee <elvis2681@gmail.com>
2021-05-15 16:52:54 -0700

    trimmed up unnecessary code

--- src/Component/EmployeeTable.js
+++ src/Component/EmployeeTable.js
@@ -8,44 +8,26 @@ class EmployeeTable extends Component {
   state = {
     results: [],
     search: "",
+    searchResults: [],
   };
 
-  // When this component mounts, search the Giphy API for pictures of kittens
   componentDidMount() {
-    this.getEmployee(50);
-  }
-
-  getEmployee = (query) => {
-    console.log(query);
-    API.search(query)
+    API.search()
       .then((res) => {
         console.log(res);
         this.setState({
           results: res.data.results,
+          searchResults: res.data.results,
         });
       })
       .catch((err) => console.log(err));
-  };
-
-  handleInputChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  // When the form is submitted, search the Giphy API for `this.state.search`
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    this.searchGiphy(this.state.search);
-  };
+  }
 
   render() {
     return (
       <div>
         <TableHeader />
-        <Search />
+        <Search></Search>
         <Table results={this.state.results} />
       </div>
     );
@@ -53,37 +35,3 @@ class EmployeeTable extends Component {
 }
 
 export default EmployeeTable;
-
-//››fake data events into a map and make them into props
-// const events = [
-//   {
-//     id: 1,
-//     name: "event 1",
-//     date: "may31,2021",
-//     Location: "san Diego",
-//   },
-//   {
-//     id: 2,
-//     name: "event 2",
-//     date: "june31,2021",
-//     Location: "San Francisco",
-//   },
-//   {
-//     id: 3,
-//     name: "event 3",
-//     date: "july31,2021",
-//     Location: "New York",
-//   },
-// ];
-
-// function EventContainer() {
-//   return (
-//     <table>
-//       <thead>
-//         <tbody>
-//           <trow></trow>
-//         </tbody>
-//       </thead>
-//     </table>
-//   );
-// }
